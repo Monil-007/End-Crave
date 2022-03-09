b72cf082d9774fc9f4dba4818eb6a9a53c9e62f7
Monil Gandhi <gandhi.monil007@gmail.com>
2022-03-09 19:11:05 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+
+import Header from './components/Layout/Header';
+import Meals from './components/Meals/Meals';
+import Cart from './components/Cart/Cart';
+import CartProvider from './store/CartProvider';
 
 function App() {
+  const [cartIsShown, setCartIsShown] = useState(false);
+
+  const showCartHandler = () => {
+    setCartIsShown(true);
+  };
+
+  const hideCartHandler = () => {
+    setCartIsShown(false);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <CartProvider>
+      {cartIsShown && <Cart onClose={hideCartHandler} />}
+      <Header onShowCart={showCartHandler} />
+      <main>
+        <Meals />
+      </main>
+    </CartProvider>
   );
 }
 
 export default App;
+
+
+
+
+
+
+
+// import React, { useState } from "react";
+// import Headerr from "./components/Layout/Header";
+// import Meals from "./components/Meals/Meals";
+// import Cart from "./components/Cart/Cart";
+// import CartProvider from "./store/CartProvider";
+
+
+
+// function App() {
+
+//   const [cartIsShown, setCartIsShown] = useState(false);
+
+//   const showCartHandler = () => {
+//     setCartIsShown(true);
+//   }
+
+//   const hideCartHandler = () => {
+//     setCartIsShown(false);
+//   }
+
+//   return (
+//     <CartProvider>
+//       {cartIsShown && <Cart onClose={hideCartHandler} />}
+//       <Headerr sCart={showCartHandler} />
+//       <main>
+//         <Meals />
+//       </main>
+//     </CartProvider>
+//     //<div>Hello krishna</div>
+//   );
+// }
+
+// export default App;
